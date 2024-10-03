d947a5cca8623e48c6c0379ff6c39fe655d73d82
Jacob Lockhart <procoder165@gmail.com>
2024-10-03 08:56:53 -0400

    added array list

--- Name-Manipulation/src/main/java/com/pluralsight/name/SliceName.java
+++ Name-Manipulation/src/main/java/com/pluralsight/name/SliceName.java
@@ -1,7 +1,7 @@
 package com.pluralsight.name;
 
 import java.util.Scanner;
-
+import java.util.ArrayList;
 
 public class SliceName {
     public static void main(String[] args) {
@@ -11,18 +11,28 @@ public class SliceName {
         System.out.println("Please enter your full name: ");
         String fullName = scan.nextLine().trim();
 
-
         //Splits into array
         String[] splitName = fullName.split(" ");
-//        //Chooses whether the name has a middle name and prints accordingly
-        if (splitName.length == 2) {
-            System.out.println("First name: " + splitName[0]);
+        ArrayList<String> list = new ArrayList<>();
+
+        //Removes empty spaces from array and adds it into a new arraylist
+        int count = 0;
+        for (int i = 0; i < splitName.length; i++) {
+            if (!splitName[i].isEmpty()) {
+                list.add(splitName[i].trim());
+            }
+        }
+
+
+        //Chooses whether the name has a middle name and prints accordingly
+        if (list.size() == 2) {
+            System.out.println("First name: " + list.get(0));
             System.out.println("Middle name: (none)");
-            System.out.println("First name: " + splitName[1]);
-        } else if (splitName.length == 3) {
-            System.out.println("First name: " + splitName[0]);
-            System.out.println("Middle name: " + splitName[1]);
-            System.out.println("Last name: " + splitName[2]);
+            System.out.println("First name: " + list.get(1));
+        } else if (list.size() == 3) {
+            System.out.println("First name: " + list.get(0));
+            System.out.println("Middle name: " + list.get(1));
+            System.out.println("Last name: " + list.get(2));
         }
     }
 }
